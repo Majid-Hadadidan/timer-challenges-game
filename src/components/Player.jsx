d326bb8fa61bb3b936bf69398cba41ec33cea166
majidH <majid.hadadian@gmail.com>
2024-03-17 12:20:24 +0330

    add useRef hook

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,20 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
+  const playerName = useRef();
   const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-  function handleChange(event) {
-    setEnteredPlayerName(event.target.value);
-  }
-  function handleClick(event) {
-    setSubmitted(true);
+
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value=''
   }
   return (
     <section id="player">
       <h2>
-        Welcome {submitted ? enteredPlayerName : "unknown entity"}
+        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
       </h2>
       <p>
-        <input type="text" value={enteredPlayerName} onChange={handleChange} />
+        <input type="text" ref={playerName} />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
